ada70c1b13dcd6d38d5568c90335505e541a395d
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-03 21:53:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,6 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length > 2 
-    x = []
-    x << array.pop
-    array << x 
+    return array.join(" and ")
   end
 end
