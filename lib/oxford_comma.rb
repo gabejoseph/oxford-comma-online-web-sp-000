ecb94c60774dec307d3b84ebbfa62f05d9a634eb
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-03 22:47:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,10 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   else
-    array.length > 3 
-    array[-1] = "and #{array[-1]}" 
-    [blah, blah, [blah]]
-    array.join(", ")
+    x = []
+    # array.length > 3 
+    # array[-1] = "and #{array[-1]}" 
+    x << array.pop
+    # array.join(", ")
   end
 end
