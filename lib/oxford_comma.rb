5fe4cc05ed58d6ed07c62d40281c1a15e2c40bb5
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-03 21:31:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2 
+    x << array.pop
+    binding.pry
+  end
 end
